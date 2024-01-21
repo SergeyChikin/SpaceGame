eda65ab31c3f121e9468dc2d3d964c46d70960ca
SergeyChikin <sergeychikin1981@gmail.com>
2024-01-21 18:54:50 +0300

    Added the FPS(frames per second) class to calculate the "time difference".

--- src/core/Entry.java
+++ src/core/Entry.java
@@ -3,5 +3,18 @@ package core;
 public class Entry {
     public static void main(String[] args) {
         Window window = new Window("SPACE GAME");
+
+        boolean runGame = true;
+
+        FPS.calcBeginTime();
+        while (runGame) {
+            // update the game state
+
+            // render the game
+//            System.out.printf("%f\n", FPS.getDeltaTime());
+
+            // recalculate delta time
+            FPS.calcDeltaTime();
+        }
     }
 }
