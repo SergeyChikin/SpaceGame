e40c4962a135a084764c630d4266e3137d120f5d
SergeyChikin <sergeychikin1981@gmail.com>
2024-01-22 22:46:57 +0300

    Add interface "Renderable", refactor class "Renderer".

--- src/render/Renderer.java
+++ src/render/Renderer.java
@@ -3,14 +3,28 @@ package render;
 import core.Window;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Renderer extends JPanel {
+    private static ArrayList<Renderable> renderableObjects = new ArrayList<Renderable>();
+
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Graphics2D graphics2D = (Graphics2D) graphics;
 
-        graphics.drawRect(100, 100, 100, 100);
+       for(Renderable object: renderableObjects)
+           object.draw(graphics2D);
+    }
+
+    public static void addRenderableObject(Renderable object) {
+        renderableObjects.add(object);
+        Collections.sort(renderableObjects);
+    }
+
+    public static void removeRenderableObject(Renderable object) {
+        renderableObjects.remove(object);
     }
 
 
