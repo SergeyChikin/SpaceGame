69febf2bb212c8e76068a4b27ccbffc06295b8eb
SergeyChikin <sergeychikin1981@gmail.com>
2024-01-29 15:36:58 +0300

    Added the "Bullet" and "Timer" classes.

---
+++ src/core/Timer.java
@@ -0,0 +1,33 @@
+package core;
+
+import update.Updateable;
+import update.Updater;
+
+import java.io.IOException;
+
+public class Timer implements Updateable {
+    int setMillisTime = 0;
+    int beginningMillisTime;
+
+    public Timer(int setMillisTime) {
+        this.beginningMillisTime = setMillisTime;
+
+        Updater.addUpdateableObject(this);
+    }
+
+    @Override
+    public void update() throws IOException {
+        setMillisTime -= FPS.getDeltaTime() * 1000;
+    }
+
+    public boolean isRinging() {
+        if (setMillisTime <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void resetTimer() {
+        setMillisTime = beginningMillisTime;
+    }
+}
---
+++ src/objects/Bullet.java
@@ -0,0 +1,80 @@
+package objects;
+
+import render.Renderable;
+import render.Renderer;
+import update.Updateable;
+import update.Updater;
+
+import core.Window;
+import core.FPS;
+
+import java.awt.image.BufferedImage;
+
+import java.io.IOException;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Bullet implements Updateable, Renderable {
+    private static double width = 10;
+    private static double height = 10;
+    private double x;
+    private double y;
+
+    private final int layer = 1;
+
+    private static BufferedImage bullet;
+
+    private static double speed = 1200;
+
+    public Bullet(double x, double y) throws IOException {
+        this.x = x - (getWidth() / 2);
+        this.y = y;
+
+        bullet = ImageIO.read(new File("res/onfire3x_87037.png"));
+
+        Renderer.addRenderableObject(this);
+        Updater.addUpdateableObject(this);
+    }
+
+    @Override
+    public int getLayer() {
+        return layer;
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public BufferedImage getBufferedImage() {
+        return bullet;
+    }
+
+    @Override
+    public void update() throws IOException {
+        y -= speed * FPS.getDeltaTime();
+
+        if (y < -getHeight()) {
+            Updater.removeUpdateableObjects(this);
+            Renderer.removeRenderableObject(this);
+        }
+    }
+
+}
