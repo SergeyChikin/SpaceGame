490e1de41848584a5a885ce4543d0a402e12aefe
SergeyChikin <sergeychikin1981@gmail.com>
2024-02-01 20:05:35 +0300

    Added the "isColliding" method to the "Updatable" class and added the "Sound" class.

--- src/update/Updateable.java
+++ src/update/Updateable.java
@@ -1,5 +1,32 @@
 package update;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import render.Renderable;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public interface Updateable {
-    public void update();
+    public void update() throws IOException, UnsupportedAudioFileException, LineUnavailableException;
+
+    public String getID();
+
+    public Renderable getRenderable();
+
+    public default Updateable isColliding(Renderable thisObject, String otherObjectID) {
+        ArrayList<Updateable> objects = Updater.getUpdateableObjects();
+        for (Updateable object : objects) {
+            if (object.getID() == otherObjectID) {
+                if (thisObject.getX() < object.getRenderable().getX() + object.getRenderable().getWidth()
+                        && thisObject.getX() + thisObject.getWidth() > object.getRenderable().getX()) {
+                    if (thisObject.getY() < object.getRenderable().getY() + object.getRenderable().getHeight()
+                            && thisObject.getY() + thisObject.getHeight() > object.getRenderable().getY()) {
+                        return object;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
